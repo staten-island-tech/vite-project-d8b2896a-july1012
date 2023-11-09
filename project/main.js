110949556e76b5e4bcfa73e301f8cd7c0a94dc72
july1012 <113192987+july1012@users.noreply.github.com>
2023-11-09 13:17:29 -0500

    adfedaef

--- project/main.js
+++ project/main.js
@@ -1,7 +1,8 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import '../style.css';
+import { tea } from './tea';
+import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
---
+++ project/tea.js
@@ -0,0 +1,52 @@
+const tea =[
+    {
+        person: "Jolene",
+        nickname: "AKA jojo",
+        age: 15,
+        img: "",
+        desc: "ugh this girl is like completely obssessed with bread.. like CLAP IF YOU'VE EVER BEEN TO A ...",
+        CANCELLED: true,
+    },
+    {
+        person: "Pamithi",
+        nickname: "AKA pami",
+        age: 15,
+        img: "",
+        desc: "spotted in staircase I.. ",
+        CANCELLED: true,
+    },
+    {
+        person: "Michael",
+        nickname: "AKA Mr. Whalen",
+        age:72,
+        img:"",
+        desc: "apparently he fakes being bald and does it for attention yikes",
+        CANCELLED: true,
+    },
+    {
+        person: "Jane",
+        nickname: "AKA janiepoo",
+        age: 15,
+        img:"",
+        desc:"Jane is perfect ",
+        CANCELLED: false,
+    },
+    {
+        person: "Ben",
+        nickname:"AKA zeppole",
+        age:16,
+        img:"",
+        desc:"he sits next to me and he secretly watches Dharmann during class instead of doing work",
+        CANCELLED: true,        
+    },
+    {
+        person:"Kai",
+        nickname:"AKA Kai",
+        age:15,
+        img:"",
+        desc:"caught walking around school with mustard inside his socks and likes it",
+        CANCELLED: false,
+    },
+];
+
+export{tea};
