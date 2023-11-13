01ad8acf62c28a10f6df649ff06bc5aa1077bf36
july1012 <113192987+july1012@users.noreply.github.com>
2023-11-13 13:08:39 -0500

    fgbdbhsxf

--- project/js/main.js
+++ project/js/main.js
@@ -1,8 +1,20 @@
-import"../css/style.css";
-import{tea} from "./tea";
-console.log(tea);
+import "../css/style.css";
+import { tea } from "./tea";
 
-function  inject(){
+function cardss (te){
+
+
+    forEach
     
-}
+("#app").insertHTML =  `
+<h1 class="header">Read the hottest tea about SI Tech..</h1>
+<h2 class="subheader">hey guys, I'm back. and I have some juicy gossip</h2>
+<div id="cards">
+    <h2 class="name">${tea.person}</h2>
+    <img src= "" class="pic">
+    <h3 class="oop"></h3>
+</div>
+`};
+
+console.log(tea);
 
--- project/js/tea.js
+++ project/js/tea.js
@@ -1,52 +1,40 @@
 const tea =[
     {
         person: "Jolene",
-        nickname: "AKA jojo",
-        age: 15,
-        img: "",
+        img: 'images/jojo.jpg',
         desc: "ugh this girl is like completely obssessed with bread.. like CLAP IF YOU'VE EVER BEEN TO A ...",
         CANCELLED: true,
     },
     {
         person: "Pamithi",
-        nickname: "AKA pami",
-        age: 15,
-        img: "",
+        img: 'images/pa,mi.jpg',
         desc: "spotted in staircase I... nothing more needs to be said. She better watch out next time.  ",
         CANCELLED: true,
     },
     {
-        person: "Michael",
-        nickname: "AKA Mr. Whalen",
-        age:72,
-        img:"",
+        person: "Mr. Whalen",
+        img:'images/whealen.jpg',
         desc: "apparently he fakes being bald and does it for attention yikes",
         CANCELLED: true,
     },
     {
         person: "Jane",
-        nickname: "AKA janiepoo",
-        age: 15,
-        img:"",
+        img:'images/jane.jpg',
         desc:"Jane is perfect ",
         CANCELLED: false,
     },
     {
         person: "Ben",
-        nickname:"AKA zeppole",
-        age:16,
-        img:"",
+        img:'images/ben.jpg',
         desc:"he sits next to me and he secretly watches Dharmann during class instead of doing work",
         CANCELLED: true,        
     },
     {
         person:"Kai",
-        nickname:"AKA Kai",
-        age:15,
-        img:"",
+        img:'images/kai.jpg',
         desc:"caught walking around school with mustard inside his socks and likes it",
         CANCELLED: false,
     },
 ];
 
-export{tea};
+export { tea };
