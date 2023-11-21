13813a6d1220658d9e4a56203b43b47335a42083
july1012 <113192987+july1012@users.noreply.github.com>
2023-11-21 12:31:26 -0500

    s,dfnsdjfnds

--- project/js/main.js
+++ project/js/main.js
@@ -2,26 +2,27 @@ import "../css/style.css";
 import "../css/lalala.css";
 import { tea } from "./tea";
 
-
-function cardss1 (){
-let app = document.querySelector("#app");
-tea.forEach((ta) => app.insertAdjacentHTML ( 
-    "beforeend",
-    `
-<div class="cards">
+function cardss1() {
+  let app = document.querySelector("#app");
+  tea.forEach((ta) =>
+    app.insertAdjacentHTML(
+      "beforeend",
+      `   <div class="cards">
     <h2 class="name">${ta.person}</h2>
     <img src= "${ta.img}" class="pic">
     <h3 class="oop">${ta.desc}</h3>
 </div>
-`))};
+`
+    )
+  );
+}
 cardss1();
 console.log(tea);
 
-const DOMSelectors ={
-    app: document.querySelector("#app")
-}
+const DOMSelectors = {
+  app: document.querySelector("#app"),
+};
 
-DOMSelectors.app.addEventListener("click", function (event){
-    event.preventDefault();
-    
-})
+DOMSelectors.app.addEventListener("click", function (event) {
+  event.preventDefault();
+});
