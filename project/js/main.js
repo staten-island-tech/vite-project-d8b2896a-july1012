6d6a65774797f67b1a077af64f9e38dd0e717eba
july1012 <113192987+july1012@users.noreply.github.com>
2023-11-30 21:16:43 -0500

    adfdsfvgs

--- project/js/main.js
+++ project/js/main.js
@@ -1,5 +1,5 @@
 import "../css/style.css";
-import "../css/lalala.css";
+
 import { tea } from "./tea";
 
 function cardss1() {
@@ -8,9 +8,9 @@ function cardss1() {
     app.insertAdjacentHTML(
       "beforeend",
       `   <div class="cards">
-    <h2 class="name">${ta.person}</h2>
+    <h3 class="name">${ta.person}</h3>
     <img src= "${ta.img}" class="pic">
-    <h3 class="oop">${ta.desc}</h3>
+    <h5 class="oop">${ta.desc}</h5>
 </div>
 `
     )
@@ -19,15 +19,12 @@ function cardss1() {
 cardss1();
 console.log(tea);
 
-const DOMSelectors = {
-  app: document.querySelector("#app"),
-};
-
-document.querySelectorAll(".Greenbutton").forEach((btn) =>
-btn.addEventListener("click", function (event) {
-  event.preventDefault();
-  
-
-}))
-
-
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("pinkk")) {
+    document.body.classList.add("pinkk");
+    document.body.classList.remove("greenn");
+  } else {
+    document.body.classList.add("greenn");
+    document.body.classList.remove("pinkk");
+  }
+});
