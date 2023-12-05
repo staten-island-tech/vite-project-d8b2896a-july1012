fc4f57435a794f6e47d9c786045f65777a5556b0
july1012 <113192987+july1012@users.noreply.github.com>
2023-12-04 22:26:12 -0500

    dfgfrse

--- project/js/tea.js
+++ project/js/tea.js
@@ -1,40 +1,41 @@
-const tea =[
-    {
-        person: "Jolene",
-        img: 'jojo.jpg',
-        desc: "ugh this girl is like completely obssessed with bread.. like CLAP IF YOU'VE EVER BEEN TO A ...",
-        CANCELLED: true,
-    },
-    {
-        person: "Pamithi",
-        img: 'pami.jpg',
-        desc: "spotted in staircase I... nothing more needs to be said. She better watch out next time.  ",
-        CANCELLED: true,
-    },
-    {
-        person: "Mr. Whalen",
-        img:'whealen.jpg',
-        desc: "I heard that Mr Whalen apparently fakes being bald and does it for attention yikes",
-        CANCELLED: true,
-    },
-    {
-        person: "Jane",
-        img:'jane.jpg',
-        desc:"Jane is perfect ",
-        CANCELLED: false,
-    },
-    {
-        person: "Ben",
-        img:'ben.jpg',
-        desc:"he sits next to me and he secretly watches Dharmann during class instead of doing work, embarrassinggg",
-        CANCELLED: true,        
-    },
-    {
-        person:"Kai",
-        img:'kai.jpg',
-        desc:"caught walking around school with mustard inside his socks and likes it, but this is his first offense so hes okay. for now.",
-        CANCELLED: false,
-    },
+const tea = [
+  {
+    person: "Jolene",
+    img: "jojo.jpg",
+    desc: "ugh this girl is like completely obssessed with bread.. like CLAP IF YOU'VE EVER BEEN TO A ...",
+    CANCELLED: true,
+  },
+  {
+    person: "Jane",
+    img: "jane.jpg",
+    desc: "Jane is perfect ",
+    CANCELLED: false,
+  },
+  {
+    person: "Pamithi",
+    img: "pami.jpg",
+    desc: "spotted in staircase I... nothing more needs to be said. She better watch out next time.  ",
+    CANCELLED: true,
+  },
+  {
+    person: "Mr. Whalen",
+    img: "whealen.jpg",
+    desc: "I heard that Mr Whalen apparently fakes being bald and does it for attention yikes",
+    CANCELLED: true,
+  },
+
+  {
+    person: "Ben",
+    img: "ben.jpg",
+    desc: "he sits next to me and he secretly watches Dharmann during class instead of doing work, embarrassinggg",
+    CANCELLED: true,
+  },
+  {
+    person: "Kai",
+    img: "kai.jpg",
+    desc: "caught walking around school with mustard inside his socks and likes it, but this is his first offense so hes okay. for now.",
+    CANCELLED: false,
+  },
 ];
 
 export { tea };
